a067df0882b18ed2dd5c4190a74e39bd4d50c132
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-11-15 18:57:52 -0300

    :construction: feat: Landing Page Layout

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,21 @@
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full border-t bg-slate-100 px-4">
+      <div className="mx-auto flex w-full items-center justify-between md:max-w-screen-2xl">
+        <Logo />
+        <div className="items=center flex w-full justify-between space-x-4 md:block md:w-auto">
+          <Button size="sm" variant="ghost">
+            <Link href="/sign-in">Privacy Policy</Link>
+          </Button>
+          <Button size="sm" variant="ghost">
+            <Link href="/sign-up">Terms of Service</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ components/logo.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import localFont from "next/font/local";
+import Image from "next/image";
+import Link from "next/link";
+
+const headingFont = localFont({
+  src: "../public/fonts/font.woff2",
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hidden items-center gap-x-2 transition hover:opacity-75 md:flex">
+        <Image src="/logo.svg" alt="Logo" width={30} height={30} />
+        <p
+          className={cn("pt-1 text-lg text-neutral-700", headingFont.className)}
+        >
+          ZenTask
+        </p>
+      </div>
+    </Link>
+  );
+};
